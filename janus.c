a8a41c8af3ab19dff0c255d6538ccf3c40e874b3
Philip Withnall <philip.withnall@collabora.co.uk>
2014-08-26 18:01:41 +0100

    janus: Don’t call g_type_init() for GLib ≥ 2.36.0
    It is no longer needed.

--- janus.c
+++ janus.c
@@ -2691,7 +2691,9 @@ gint main(int argc, char *argv[])
 	signal(SIGINT, janus_handle_signal);
 
 	/* Setup Glib */
+#if !GLIB_CHECK_VERSION(2, 36, 0)
 	g_type_init();
+#endif
 	
 	/* Logging level: default is info */
 	log_level = LOG_INFO;
